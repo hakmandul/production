8ca0f83645014a69035acde9d22aa8cdeb858670
hakmandul <ludmilagakman03@gmail.com>
2026-01-16 01:14:38 +0200

    commit

--- frontend/app/pages/check.vue
+++ frontend/app/pages/check.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
 import { authClient } from "~/lib/auth-client";
-const { data: session } = await authClient.useSession(useFetch);
+
+// Убираем useFetch, оставляем стандартный клиентский запрос
+const session = authClient.useSession(); 
 </script>
+
 <template>
-    <p>
-        {{ session }}
-    </p>
+    <div class="p-5">
+        <!-- Показываем "Загрузка...", пока данные идут -->
+        <div v-if="session.isPending" class="text-gray-500">Загрузка...</div>
+        
+        <pre v-else-if="session.data">{{ session.data }}</pre>
+        
+        <div v-else>Вы не авторизованы</div>
+    </div>
 </template>
